2ceb06026dc62b5d196ee209ef597c713e45a519
ebo <manduuyla@yahoo.com>
2021-11-21 00:07:28 -0500

    assets added and more components

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './header.css'
+
+function Header() {
+    return (
+        <section className='nav-section'>
+            <nav className='nav'>
+                <img src='' alt='mps-logo' className='nav-logo' />
+                <ul className='nav-ul'>
+                    <li className='nav-li'>About</li>
+                    <li className='nav-li'>Services</li>
+                    <li className='nav-li'>Gallery</li>
+                    <li className='nav-li'>Contact</li>
+                </ul>
+            </nav>
+        </section>
+    )
+}
+
+export default Header;
