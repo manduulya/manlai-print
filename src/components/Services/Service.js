17c7bcace5d02344a6dab08b4acce51d85ca12f5
ebo <manduuyla@yahoo.com>
2021-12-20 21:37:13 -0500

    font changed more styling and service component added

---
+++ src/components/Services/Service.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Service.css'
+
+const Service = () => {
+    console.log('function component')
+    return (
+        <section className='service-section-wrapper'>
+            <h3 className='service-header'>Hello</h3>
+            <p className='service-content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum orci nec ultricies semper.
+                Sed scelerisque turpis nec lobortis consequat. Sed enim nibh, efficitur luctus purus vel, aliquam
+                posuere sapien. Mauris commodo varius odio, id rhoncus felis porttitor vel. Integer tincidunt nisl
+                sit amet est posuere fringilla. Nulla nec dui facilisis, aliquam magna sit amet, maximus mi. </p>
+        </section>
+    )
+}
+
+
+export default Service;
